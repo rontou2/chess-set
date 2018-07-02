6939561674772cab2efe18f7efe7ae4d8aaab4f4
Devon Bacon <devon.bacon@gmail.com>
2018-07-02 11:38:29 -0700

    App.js - imported game class and set up constructor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      game: new Game(),
+    };
+
+    this.state.game.setBoard();
+  }
+  
   render() {
     return (
       <div className="App">
